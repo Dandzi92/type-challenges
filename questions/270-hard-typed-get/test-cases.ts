adccdab922726f0d6c3bd3e1ea02d75145e77f1b
Anthony Fu <11247099+antfu@users.noreply.github.com>
2020-09-26 13:58:24 +0000

    feat(question): add #270 - Typed Get

---
+++ questions/270-hard-typed-get/test-cases.ts
@@ -0,0 +1,21 @@
+import { Equal, Expect, ExpectFalse, NotEqual } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<Get<Data, 'hello'>, 'world'>>,
+  Expect<Equal<Get<Data, 'foo.bar.count'>, 6>>,
+  Expect<Equal<Get<Data, 'foo.bar'>, { value: 'foobar', count: 6 }>>,
+]
+
+// @ts-expect-error
+type Error = get<Data, 'no.existed'>
+
+type Data = {
+  foo: {
+    bar: {
+      value: 'foobar',
+      count: 6,
+    },
+    included: true,
+  },
+  hello: 'world'
+}
